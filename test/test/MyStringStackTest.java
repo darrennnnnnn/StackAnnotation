90a61c55ba0cc9f5218b152792ebb4d1e1a978a5
Darren Toh (NCS) <darren.toh@ncs.com.sg>
2020-08-25 14:05:53 +0800

    fail the test case

--- test/test/MyStringStackTest.java
+++ test/test/MyStringStackTest.java
@@ -36,8 +36,6 @@ public class MyStringStackTest extends TestCase {
 		stack.push (s1);
         assertTrue (stack.isEmpty());
 		stack.push (s1);
-        stack.push (s2);
-        assertFalse (stack.isEmpty());
 	}
 
 	@Test
